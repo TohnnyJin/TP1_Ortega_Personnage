9a956dbf564401b12f5ece2dc170be05b07b7b27
TohnnyJin <98454836+TohnnyJin@users.noreply.github.com>
2022-01-26 10:16:51 +0100

    Add files via upload

---
+++ jeuPersonnage.java
@@ -0,0 +1,13 @@
+import Personnage.*;
+public class jeuPersonnage{
+public static void main (String args[]){
+Personnage UnSorcier = new Personnage ("Harry Potter", 100, 15);
+System.out.println("Votre personnage est : "+ UnSorcier.LeNom() );
+System.out.println("Il possède : "+ UnSorcier.LaVie() +" points de vie.");
+System.out.println("Il possède aussi : "+ UnSorcier.LaForce() +" points d'attaque.");
+
+Personnage victime = new Personnage ("jacques", 80, 1);
+
+
+}
+}
